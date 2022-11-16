8526fc3d3951720911b5d9be5fcdabcec1cab995
htlWels <franz.reitinger@htl-wels.at>
2022-11-16 18:37:56 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style>
-#app {
+#app1 {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
